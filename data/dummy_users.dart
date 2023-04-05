e05e0de08014ec7ee4f425b7113737b67db68a44
Leoz_in <leoz_in@hotmail.com>
2023-04-04 21:10:54 -0300

    crud de login em criacao

---
+++ data/dummy_users.dart
@@ -0,0 +1,51 @@
+import 'package:cadastro/models/user.dart';
+
+const DUMMY_USERS = {
+'1' : const User(
+id:'1',
+name: 'Shi Okohan',
+email:'shi_oko@senac.br',
+avatarUrl:
+'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
+),
+
+'2' : const User (
+id:'2',
+name: 'Ruy Volweasley',
+email:'ruy@senac.br',
+avatarUrl:
+'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+),
+
+'3' : const User (
+id:'3',
+name: 'Tom Cruiz',
+email:'oimpossivel@senac.br',
+avatarUrl:
+'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png',
+),
+
+'4' : const User (
+id:'4',
+name: 'Ruivinha de Marte',
+email:'salgadeiradap3@senac.br',
+avatarUrl:
+'https://cdn.pixabay.com/photo/2017/03/01/22/18/avatar-2109804_960_720.png',
+),
+
+'5' : const User (
+id:'5',
+name: 'Mariele',
+email:'marirj@senac.br',
+avatarUrl:
+'https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png',
+),
+
+'6' : const User (
+id:'5',
+name: 'Willian Smith',
+email:'mib@senac.br',
+avatarUrl:
+'https://cdn.pixabay.com/photo/2017/01/31/21/22/avatar-2027363_960_720.png',
+),
+};
